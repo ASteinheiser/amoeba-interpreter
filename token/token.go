71ebd9d2b4443ca283bf5c4dd00ba954c84262e9
Andrew <me@iamandrew.io>
2019-09-05 21:45:17 -0700

    create token pkg

---
+++ token/token.go
@@ -0,0 +1,42 @@
+package token
+
+// Type is the Token's type identifier
+type Type string
+
+// Token is a single element
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+const (
+	// ILLEGAL : bad token
+	ILLEGAL = "ILLEGAL"
+	// EOF : end of file
+	EOF = "EOF"
+	// IDENT : identifiers or variables
+	IDENT = "IDENT"
+	// INT : integers
+	INT = "INT"
+	// ASSIGN : sets an identifier equal to a literal
+	ASSIGN = "="
+	// PLUS : adds two integers
+	PLUS = "+"
+	// COMMA : separator for elements in a list
+	COMMA = ","
+	// SEMICOLON : ends a line of code
+	SEMICOLON = ";"
+	// LPAREN : start listing function call params
+	LPAREN = "("
+	// RPAREN : stop listing function call params
+	RPAREN = ")"
+	// LBRACE : open new block of code
+	LBRACE = "{"
+	// RBRACE : close block of code
+	RBRACE = "}"
+	// FUNCTION : create a function that accepts
+	// params and returns a value
+	FUNCTION = "FUNCTION"
+	// LET : initialize a new identifier
+	LET = "LET"
+)
