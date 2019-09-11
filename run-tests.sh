77ac090638582dbf0e9ecb343954c6b03b2ecce2
Andrew <me@iamandrew.io>
2019-09-11 00:14:27 -0700

    improve test script

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+Purple='\033[0;35m'
+BPurple='\033[1;35m'
+Cyan='\033[0;36m' 
+BCyan='\033[1;36m'
+NoColor='\033[0m'
+
+echo -e "${BCyan}Running the Amoeba test suite!${NoColor}"
+echo ""
+echo -e "${BPurple}¯\_(ツ)_/¯${NoColor}"
+echo ""
+
+echo -e "${Cyan}Lexer Test Results:${NoColor}"
 go test ./lexer/
+echo ""
+
+echo -e "${Cyan}Parser Test Results:${NoColor}"
 go test ./parser/
+echo ""
