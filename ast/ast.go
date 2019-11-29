b8c78d8470a3a962de6dd4349c65ff73ec367d5c
Andrew <me@iamandrew.io>
2019-11-28 22:44:46 -0700

    add function literal to AST, make failing parser tests

--- ast/ast.go
+++ ast/ast.go
@@ -4,6 +4,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/ASteinheiser/amoeba-interpreter/token"
 )
@@ -259,6 +260,35 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+
+	return out.String()
+}
+
+// FunctionLiteral is an Expression Node representing a function
+type FunctionLiteral struct {
+	Token      token.Token // should be an 'fn' token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+
+// TokenLiteral returns the token literal for the if
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -632,3 +632,55 @@ func TestIfElseExpression(t *testing.T) {
 		return
 	}
 }
+
+func TestFunctionLiteral(t *testing.T) {
+	input := `
+		fn(x, z) {
+			x * z;
+		}
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("expected program to have 1 statement, got=%d",
+			len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not a *ast.ExpressionStatement, got=%T",
+			program.Statements[0])
+	}
+
+	function, ok := stmt.Expression.(*ast.FunctionLiteral)
+	if !ok {
+		t.Fatalf("stmt.Expression is not a *ast.FunctionLiteral, got=%T",
+			stmt.Expression)
+	}
+
+	if len(function.Parameters) != 2 {
+		t.Fatalf("function.Parameters not equal to 2, got=%d\n",
+			len(function.Parameters))
+	}
+
+	testLiteralExpression(t, function.Parameters[0], "x")
+	testLiteralExpression(t, function.Parameters[1], "z")
+
+	if len(function.Body.Statements) != 1 {
+		t.Fatalf("function.Body.Statements not equal to 1, got=%d\n",
+			len(function.Parameters))
+	}
+
+	bodyStmt, ok := function.Body.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("function body statement is not a *ast.ExpressionStatement, got=%T",
+			function.Body.Statements[0])
+	}
+
+	testInfixLiteral(t, bodyStmt.Expression, "x", "*", "z")
+}
