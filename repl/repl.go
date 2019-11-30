e205272a198d8cfcff6431deef1cdd779afe6d7a
Andrew <me@iamandrew.io>
2019-11-30 11:20:01 -0700

    add amoeba ascii art to repl

--- repl/repl.go
+++ repl/repl.go
@@ -10,19 +10,6 @@ import (
 	"github.com/ASteinheiser/amoeba-interpreter/parser"
 )
 
-// AMOEBA is an ascii amoeba string
-const AMOEBA = ``
-
-// ShowPrompt represents the symbols in the amoeba REPL
-// directly before where the user types
-func ShowPrompt() {
-	color.ChangeColor(color.Magenta, false, color.Black, false)
-	fmt.Print(` ¯\_(ツ)_/¯`)
-	color.ChangeColor(color.White, true, color.Black, false)
-	fmt.Print(" >>>> ")
-	color.ResetColor()
-}
-
 // Start will start a new amoeba REPL
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -50,10 +37,18 @@ func Start(in io.Reader, out io.Writer) {
 	}
 }
 
+// ShowPrompt prints out the symbols in the amoeba REPL
+// directly before where the user types
+func ShowPrompt() {
+	color.ChangeColor(color.Magenta, false, color.Black, false)
+	fmt.Print(` ¯\_(ツ)_/¯`)
+	color.ChangeColor(color.White, true, color.Black, false)
+	fmt.Print(" >>>> ")
+	color.ResetColor()
+}
+
 func printParserErrors(out io.Writer, errors []string) {
-	io.WriteString(out, "\n")
-	io.WriteString(out, AMOEBA)
-	io.WriteString(out, "\n")
+	ShowAmoeba(out)
 	io.WriteString(out, "  Oops! Looks like your syntax got infected...\n")
 	io.WriteString(out, "    parser errors:\n\n")
 
@@ -61,5 +56,32 @@ func printParserErrors(out io.Writer, errors []string) {
 	for _, msg := range errors {
 		io.WriteString(out, "      "+msg+"\n\n")
 	}
+	color.ResetColor()
+}
+
+// ShowAmoeba prints the ASCII amoeba
+func ShowAmoeba(out io.Writer) {
+	color.Foreground(color.Green, false)
+	io.WriteString(out, "\n")
+	io.WriteString(out, "                     ,,emmw,\n")
+	io.WriteString(out, "                 a#M7|``  `^\"#p\n")
+	io.WriteString(out, "               #M`            #p\n")
+	io.WriteString(out, "              #b              @#\n")
+	io.WriteString(out, "              #b              ]#\n")
+	io.WriteString(out, "              \"#        ####m  @Q           ,e###M##m\n")
+	io.WriteString(out, "               \"#      #######  7W#Mmm###M57^`      '@b\n")
+	io.WriteString(out, "                \"@N    @######b               a#Mm    #p\n")
+	io.WriteString(out, "                  7@w   @#####`              #b  #~   @#\n")
+	io.WriteString(out, "                    %#   \"%##\"               \"%WM\\    @#\n")
+	io.WriteString(out, "                     @b                             ,##\n")
+	io.WriteString(out, "                     @#                   a###W%55577`\n")
+	io.WriteString(out, "                    ,#=                 ##|\n")
+	io.WriteString(out, "                   ;#\"    a#Mm         #b\n")
+	io.WriteString(out, "                  ##p    #b  #~       #M\n")
+	io.WriteString(out, "                  #b     \"%WM\\       #b\n")
+	io.WriteString(out, "                  @#              ,##\"\n")
+	io.WriteString(out, "                   %#p         ,##C,\n")
+	io.WriteString(out, "                     7%@#####WT^`\n")
+	io.WriteString(out, "\n")
 	color.ResetColor()
 }
