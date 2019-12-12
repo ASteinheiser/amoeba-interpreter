37b73871a7bdbbc96b1a18bd0ddb5b3ee69796a2
Andrew <me@iamandrew.io>
2019-12-11 21:41:22 -0700

    evaluate boolean comparisons

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -93,6 +93,10 @@ func evalInfixExpression(
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
 	default:
 		return NULL
 	}
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -72,6 +72,15 @@ func TestEvalBooleanExpression(t *testing.T) {
 		{"1 != 1", false},
 		{"1 == 2", false},
 		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false == true", false},
+		{"(1 < 2) == true", true},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
+		{"(1 > 2) == true", false},
 	}
 
 	for _, test := range tests {
