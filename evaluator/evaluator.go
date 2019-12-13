a648b521c6896c763387cc10c7e62174fbf021da
Andrew <me@iamandrew.io>
2019-12-12 22:33:14 -0700

    evaluate if else expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -20,6 +20,8 @@ func Eval(node ast.Node) object.Object {
 	// Statements
 	case *ast.Program:
 		return evalStatements(node.Statements)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	// Expressions
@@ -34,6 +36,8 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
@@ -128,5 +132,30 @@ func evalIntegerInfixExpression(
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case FALSE:
+		return false
+	case TRUE:
+		return true
+	default:
+		return true
 	}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -122,3 +122,36 @@ func TestEvalBangOperator(t *testing.T) {
 		testBooleanObject(t, evaluated, test.expected)
 	}
 }
+
+func TestEvalIfElseExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if(true) { 10 }", 10},
+		{"if(false) { 10 }", nil},
+		{"if(1) { 10 }", 10},
+		{"if(1 < 2) { 10 }", 10},
+		{"if(1 > 2) { 10 }", nil},
+		{"if(1 > 2) { 10 } else { 20 }", 20},
+		{"if(1 < 2) { 10 } else { 20 }", 10},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		integer, ok := test.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("obj is not NULL, got=%T (%+v)", obj, obj)
+		return false
+	}
+	return true
+}
