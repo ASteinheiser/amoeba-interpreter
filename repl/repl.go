ba41e581855df4d18cb789eb0ce2791668485f7b
Andrew <me@iamandrew.io>
2020-04-06 11:09:16 -0700

    format program output

--- repl/repl.go
+++ repl/repl.go
@@ -40,7 +40,7 @@ func Start(in io.Reader, out io.Writer) {
 
 		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
-			io.WriteString(out, "\n  ")
+			io.WriteString(out, "\n")
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n\n")
 		}
